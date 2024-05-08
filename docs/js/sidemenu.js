f02ae5db4fbbbca2d64ebc73c2f8c4f70d928d20
Krzysztof Jamróz <kjamroz83@gmail.com>
2024-05-08 10:27:26 +0200

    add sidemenu links

---
+++ docs/js/sidemenu.js
@@ -0,0 +1,16 @@
+function addMenuLink(title, file) {
+  let isActive = window.location.pathname.split('/').pop() === file
+  return $('#side-menu').append(`
+    <li class="nav-item">
+      <a href="./${file}" class="nav-link ${isActive ? 'active' : 'text-white'}">
+        ${title}
+      </a>
+    </li>`) 
+}
+
+
+$(document).ready(function() {
+  $('#side-title').text('Crypt 101')
+  addMenuLink('Welcome', 'index.html')
+})
+
